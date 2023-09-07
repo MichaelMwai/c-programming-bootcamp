678bbd36c49b4928ef27500823305f66435ba2d5
MichaelMwai <michaeldmwai@gmail.com>
2023-09-07 16:39:34 +0300

    Compiled program

--- average-grade/averageGrade.c
+++ average-grade/averageGrade.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-    int grade1, grade2, grade3;
+    int grade1, grade2, grade3; //3 codes needed for calculations
 
     printf ("What is your first grade?");
     scanf ("%d", &grade1);
@@ -12,7 +12,7 @@ int main() {
     printf("What is your third grade?");
     scanf("%d", &grade3);
 
-    double averageGrade = ((double)grade1 + grade2 + grade3)/3;
+    double averageGrade = ((double)grade1 + grade2 + grade3)/3; //casting result into a double for EXACT average
     printf ("Your average grade is %lf", averageGrade);
 
     return 0;
