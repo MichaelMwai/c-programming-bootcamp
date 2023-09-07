58835be1acfc4674bd332e00bd28d952f4de4136
MichaelMwai <michaeldmwai@gmail.com>
2023-09-07 18:20:27 +0300

    Complete program calculates a rectangles perimeter

--- rectangles-perimeter/rectangles-perimeter.c
+++ rectangles-perimeter/rectangles-perimeter.c
@@ -3,5 +3,17 @@
 
 int main(){
 
+    //Perimeter = 2L+2W
+
+    float length, width, perimeter;
+
+    printf("What is the length of the rectangle\n");
+    scanf("%f", &length);
+    printf("What is the width of the rectangle\n");
+    scanf("%f", &width);
+
+    perimeter = (2*length) + (2*width); // or (length +length + width + width)
+
+    printf ("The perimeter of the rectangle is %f\n", perimeter);
     return 0;
 }
